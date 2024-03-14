953a46f58a51d637c32aef82375fc081452ec016
黄智强 <huangzq@dustess.com>
2024-03-14 16:53:26 +0800

    feat: 使用路由

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const Koa = require("koa");
 const ip = require("ip");
 const { bodyParser } = require("@koa/bodyparser");
+const Router = require("@koa/router");
 
 const app = new Koa();
+
+const router = new Router();
 
 // koaBodyParser: 将 string 类型的 body 参数转为 json 格式 - 中间件
 app.use(bodyParser());
 
-// 响应 - 中间件
-app.use((ctx) => {
-  console.log("[ ctx.request.body ] >", ctx.request.body);
-  ctx.body = "Hello Koa";
+router.get("/", (ctx) => {
+  ctx.body = "helloworld";
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log(
