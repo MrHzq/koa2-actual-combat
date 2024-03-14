31ad7759bbc3a17869a46309d0ae053b1de2961a
黄智强 <huangzq@dustess.com>
2024-03-14 16:23:15 +0800

    feat: 使用 @koa/bodyparser

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
-import Koa from "koa";
+const Koa = require("koa");
+const ip = require("ip");
+const { bodyParser } = require("@koa/bodyparser");
 
 const app = new Koa();
 
-// 响应
+// koaBodyParser: 将 string 类型的 body 参数转为 json 格式 - 中间件
+app.use(bodyParser());
+
+// 响应 - 中间件
 app.use((ctx) => {
+  console.log("[ ctx.request.body ] >", ctx.request.body);
   ctx.body = "Hello Koa";
 });
 
 app.listen(3000, () => {
-  console.log("server is running at port http://localhost:3000");
+  console.log(
+    `server is running: http://${ip.address()}:3000 or http://localhost:3000`
+  );
 });
